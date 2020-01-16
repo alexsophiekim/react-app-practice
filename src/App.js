8371a793ca5bc96d5f31f5ca2534be664d4c9916
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2020-01-16 13:24:00 +1300

    create and read

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
+import Control from './components/Control';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id=3;
     this.state = {
-      mode:'read',
+      mode:'create',
       selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React!!'},
@@ -21,10 +24,11 @@ class App extends Component {
   }
   render() {
     console.log('App render');
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
       var i = 0;
       while(i<this.state.contents.length){
@@ -36,6 +40,22 @@ class App extends Component {
         }
         i = i +1;
       }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if (this.state.mode === 'create') {
+      _article = <CreateContent onSubmit={function(_title,_desc){
+          //add content to this.state.contents
+          this.max_content_id = this.max_content_id+1;
+          // this.state.contents.push(
+          //   {id:this.max_content_id, title:_title, desc:_desc}
+          // );
+          var _contents = this.state.contents.concat(
+            {id:this.max_content_id, title:_title, desc:_desc}
+          )
+          this.setState({
+            contents:_contents
+          });
+          console.log(_title, _desc);
+      }.bind(this)}></CreateContent>
     }
     console.log('render',this);
     return (
@@ -57,7 +77,13 @@ class App extends Component {
             });
           }.bind(this)}
           data={this.state.contents}></Nav>
-        <Content title={_title} desc={_desc}></Content>
+        <Control onChangeMode={function(_mode){
+              this.setState({
+                mode:_mode
+              })
+          }.bind(this)}>
+          </Control>
+        {_article}
       </div>
     )
   }
